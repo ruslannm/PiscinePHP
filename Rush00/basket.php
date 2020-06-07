abdaadeaeff60a360dbb7c9a2b4bc9bd08da8432
Папа <papa@iMac-Ruslan.local>
2020-06-07 20:15:25 +0300

    basket

--- Rush00/basket.php
+++ Rush00/basket.php
@@ -3,7 +3,7 @@ session_start();
 $basket_price = 0;
 foreach ($_COOKIE as $key=>$item) {
 	$product = explode("_", $key);
-	if ($product[0] == "id") {
+	if ($product[0] == "id" && $item) {
 		$tab = unserialize(file_get_contents('./db/products'));
 
 		foreach ($tab as $key => $value) {
@@ -15,6 +15,37 @@ foreach ($_COOKIE as $key=>$item) {
 		}
 	}
 }
+if ($_POST['submit'] == 'Order') {
+	$path = "./db";
+	$file_orders = $path . "/orders";
+	$file_order_items = $path . "/order_items";
+	$file_counters = $path . "/counter";
+	$fd_orders = fopen($file_orders, 'c+');
+	$fd_order_items = fopen($file_order_items, 'c+');
+	$fd_counters = fopen($file_counters, 'c+');
+	if (flock($fd_orders, LOCK_SH | LOCK_EX) && flock($fd_order_items, LOCK_SH | LOCK_EX) && flock($fd_counters, LOCK_SH | LOCK_EX)) {
+		$tab_orders = unserialize(file_get_contents($file_orders));
+		$tab_order_items = unserialize(file_get_contents($file_order_items));
+		$tab_counters = unserialize(file_get_contents($file_counters));
+		$tab_orders[] = array("id" => $tab_counters[0]["order_id"], "user" => $_SESSION["loggued_on_user"], 'time' => time(), 'price' => $basket_price);
+		file_put_contents($file_orders, serialize($tab_orders));
+		foreach ($basket as $value)
+        {
+			$tab_order_items[] = array("order_id" => $tab_counters[0]["order_id"], "product_id"=>$value[0], "count"=>$value[2],"price"=>$value[3],"total_price" =>$value[4]);
+			setcookie('id_'. $value[0], 0);
+        }
+		file_put_contents($file_order_items, serialize($tab_order_items));
+		$tab_counters[0]["order_id"] = $tab_counters[0]["order_id"] + 1;
+		file_put_contents($file_counters, serialize($tab_counters));
+		flock($fd_orders, LOCK_UN);
+		flock($fd_order_items, LOCK_UN);
+		flock($fd_counters, LOCK_UN);
+	}
+	fclose($fd_orders);
+	fclose($fd_order_items);
+	fclose($fd_counters);
+	header("Location: index.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,22 +56,23 @@ foreach ($_COOKIE as $key=>$item) {
 <body>
 <?php
 include_once ("top.php");
-?>
-<table>
-    <caption>Состав корзины</caption>
-    <tr> <td>Продукт</td> <td>Количество</td> <td>Цена</td> <td>Сумма</td></tr>
-    <?php
+    if ($basket){
+    echo '<table>  <caption>Состав корзины</caption>';
+    echo '<tr> <td>Продукт</td> <td>Количество</td> <td>Цена</td> <td>Сумма</td></tr>';
     foreach ($basket as $item)
        	echo "<tr><td><a href='product.php?id=".$item[0]."'>".$item[1].'</a></td><td>'.$item[2].'</td> <td>'.$item[3].'</td><td>'.$item[4].'</td></tr>';
     echo '<tr><td></td> <td></td> <td>Всего:</td> <td>'.$basket_price.'</td> </tr>';
     echo '</table>';
-if($_SESSION['loggued_on_user']) {
-	echo '<form action="" method="POST">';
-	echo 'Оформить заказ<input type="submit" name="submit" value="Order" />';
-	echo '</form>';
-}
+    if($_SESSION['loggued_on_user']) {
+	    echo '<form action="" method="POST">';
+	    echo 'Оформить заказ<input type="submit" name="submit" value="Order" />';
+	    echo '</form>';
+    }
+    else
+	    echo 'Для оформления заказа необходимо авторизоваться';
+    }
 else
-	echo 'Для оформления заказа необходимо авторизоваться';
+	echo '<H1>Корзина пустая</H1>';
 ?>
 </body>
 </html>
--- Rush00/install.php
+++ Rush00/install.php
@@ -14,12 +14,12 @@
 	file_put_contents($file, serialize($tab));
 	$tab = null;
 	for($i = 0; $i < 10; $i++)
-		$tab[] = array('id' => $i, 'name' => 'Товар'.$i, 'price' => 10);
+		$tab[] = array('id' => $i, 'name' => 'Товар'.$i, 'price' => 10, 'deleted'=>0);
 	$file = $path."/products";
 	file_put_contents($file, serialize($tab));
 	$tab = null;
 	for($i = 0; $i < 5; $i++)
-		$tab[] = array('id' => $i, 'name' => 'Категория'.$i);
+		$tab[] = array('id' => $i, 'name' => 'Категория'.$i, 'deleted'=>0);
 	$file = $path."/categorys";
 	file_put_contents($file, serialize($tab));
 	$tab = null;
@@ -32,8 +32,6 @@
 		}
 	$file = $path."/cat_prod";
 	file_put_contents($file, serialize($tab));
-	$file = $path."/orders_count";
-	file_put_contents($file, 1);
 	$tab = null;
 	$tab[] = array('id' => 1, 'user' => 'root', 'time'=>time(), 'price' => 10);
 	$file = $path."/orders";
@@ -41,6 +39,10 @@
 	$tab = null;
 	$tab[] = array('order_id' => 1, 'product_id' => 1, 'count' => 1 ,'price' => 10, 'total_price' => 10);
 	$file = $path."/order_items";
+	file_put_contents($file, serialize($tab));
+	$tab = null;
+	$tab[] = array('order_id' => 2, 'category_id' =>5, 'product_id' => 10);
+	$file = $path."/counter";
 	file_put_contents($file, serialize($tab));
 	header("Location: index.html");
 ?>
